5ac86642081c46fcd278fd1f4df903d0751c9e2f
Sayed <kazi-abu.sayed@capgemini.com>
2021-02-12 12:05:59 +0100

    8# APP-Routing Child Route

--- APM-Route/src/app/products/product.module.ts
+++ APM-Route/src/app/products/product.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
 import { ProductResolver } from './product-resolver.service';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
@@ -13,13 +15,20 @@ import { RouterModule } from '@angular/router';
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
       { path: 'products/:id', component: ProductDetailComponent, resolve: { resolvedData: ProductResolver } },
-      { path: 'products/:id/edit', component: ProductEditComponent, resolve: { resolvedData: ProductResolver } }
+      { path: 'products/:id/edit', component: ProductEditComponent, resolve: { resolvedData: ProductResolver },
+        children: [
+          { path: '', redirectTo: 'info', pathMatch: 'full' },
+          { path: 'info', component: ProductEditInfoComponent },
+          { path: 'tags', component: ProductEditTagsComponent }
+        ]}
     ])
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ]
 })
 export class ProductModule { }
