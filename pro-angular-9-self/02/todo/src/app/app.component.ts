5ed79d9276e654a7956568ec518ef2de4300f36d
Sayed <kazi-abu.sayed@capgemini.com>
2021-03-01 18:41:35 +0100

    pro-angular-9-self finished

--- pro-angular-9-self/02/todo/src/app/app.component.ts
+++ pro-angular-9-self/02/todo/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TodoList } from "./todoList";
+import { TodoItem } from "./todoItem";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  private list = new TodoList("Bob", [
+    new TodoItem("Go for run", true),
+    new TodoItem("Get flowers"),
+    new TodoItem("Collect tickets"),
+  ]);  
+  
+  get username(): string {
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.items.length;
+  }
+
+  get items(): readonly TodoItem[] {
+    return this.list.items.filter(item => this.showComplete || !item.complete);
+  }
+
+  addItem(newItem): void {
+    if (newItem != '') {
+      this.list.addItem(newItem);
+    }
+  }
+
+  showComplete: boolean = false;
 }
